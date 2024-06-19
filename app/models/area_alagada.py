afd21d3385e34bd31e3d2a98699bfeb670a46514
Emmanuelle <manu-benedett@live.com>
2024-06-19 02:59:17 -0300

    confirm flooded area

--- app/models/area_alagada.py
+++ app/models/area_alagada.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime, timezone
 
 
 class AreaAlagada(db.Model):
@@ -7,12 +8,16 @@ class AreaAlagada(db.Model):
     logradouro = db.Column(db.String)
     cidade = db.Column(db.String)
     bairro = db.Column(db.String)
-    descricao = db.Column(db.String)
-    cep = db.Column(db.String)
+    descricao = db.Column(db.String, default='')
+    cep = db.Column(db.String, default='')
+    data = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    area_confirmada = db.Column(db.Boolean, default=False)
 
-    def __init__(self, logradouro, cidade, bairro, descricao, cep):
+    def __init__(self, logradouro, cidade, bairro, descricao=None, cep=None, data=None, area_confirmada=False):
         self.logradouro = logradouro
         self.cidade = cidade
         self.bairro = bairro
-        self.descricao = descricao
-        self.cep = cep
+        self.descricao = descricao if descricao is not None else ''
+        self.cep = cep if cep is not None else ''
+        self.data = data if data is not None else datetime.now(timezone.utc)
+        self.area_confirmada = area_confirmada
--- app/routes/area_alagada_routes.py
+++ app/routes/area_alagada_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from datetime import datetime, timezone
 from app import db
 from app.models.area_alagada import AreaAlagada
 
@@ -8,12 +9,58 @@ area_alagada_routes = Blueprint('area_alagada_routes', __name__)
 def cadastraArea():
     data = request.get_json()
     nova_area = AreaAlagada(**data)
+    
+    area_confirmada_existente = AreaAlagada.query.filter(
+        db.func.date(AreaAlagada.data) == db.func.date(datetime.now(timezone.utc)),
+        AreaAlagada.bairro == nova_area.bairro,
+        AreaAlagada.cidade == nova_area.cidade,
+        AreaAlagada.area_confirmada == True
+    ).first()
+    
+    if area_confirmada_existente:
+        nova_area.area_confirmada = True
+    else:
+        existing_areas = AreaAlagada.query.filter(
+            db.func.date(AreaAlagada.data) == db.func.date(datetime.now(timezone.utc)),
+            AreaAlagada.bairro == nova_area.bairro,
+            AreaAlagada.cidade == nova_area.cidade
+        ).all()
+        
+        if len(existing_areas) >= 4:
+            for area in existing_areas:
+                area.area_confirmada = True
+            nova_area.area_confirmada = True
+    
     db.session.add(nova_area)
     db.session.commit()
+    
     return jsonify({'message': 'Área cadastrada com sucesso!'}), 201
+
+
+@area_alagada_routes.route('/confirmarAreaAlagada', methods=['PUT'])
+def confirmarAreaAlagada():
+    data = request.get_json()
+    cidade = data['cidade']
+    bairro = data['bairro']
+    
+    areas_to_confirm = AreaAlagada.query.filter(
+        db.func.date(AreaAlagada.data) == db.func.date(datetime.now(timezone.utc)),
+        AreaAlagada.cidade == cidade,
+        AreaAlagada.bairro == bairro,
+        AreaAlagada.area_confirmada == False
+    ).all()
+    
+    if areas_to_confirm:
+        for area in areas_to_confirm:
+            area.area_confirmada = True
+        db.session.commit()
+        return jsonify({'message': 'Áreas confirmadas com sucesso!'}), 200
+    else:
+        return jsonify({'message': 'Nenhuma área para confirmar.'}), 404
+
 
 
 @area_alagada_routes.route("/listaAreaAlagada")
 def listaAreaAlagada():
     areas_alagadas = AreaAlagada.query.all()
-    return jsonify([{'id': area._id, 'logradouro': area.logradouro, 'cidade': area.cidade, 'bairro': area.bairro, 'descricao': area.descricao, 'cep': area.cep} for area in areas_alagadas])
+    return jsonify([{'id': area._id, 'logradouro': area.logradouro, 'cidade': area.cidade, 'bairro': area.bairro, 'descricao': area.descricao, 'cep': area.cep, 'data': area.data, 'area_confirmada': area.area_confirmada} for area in areas_alagadas])
